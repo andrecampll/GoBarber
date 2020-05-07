5c47df7a13a1873323542205933d03d7b5241e1f
André Victor <andrevictor50@gmail.com>
2020-05-06 21:24:30 -0300

    finishing authenticate tests

--- src/modules/users/services/CreateSessionService.spec.ts
+++ src/modules/users/services/CreateSessionService.spec.ts
@@ -1,5 +1,4 @@
-// import AppError from '@shared/errors/AppError';
-
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/hashProvider/fakes/FakeHashProvider';
 import CreateSessionService from './CreateSessionService';
@@ -33,5 +32,50 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should be not able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createSession = new CreateSessionService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      createSession.execute({
+        email: 'test@test.com',
+        password: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password ', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const createSession = new CreateSessionService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'Infinity Test',
+      email: 'test@test.com',
+      password: '12345678',
+    });
+
+    expect(
+      createSession.execute({
+        email: 'test@test.com',
+        password: 'wrong password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
