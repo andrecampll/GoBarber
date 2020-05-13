93786b55e9ffae6c4ba47ec950d1f804aa8931a9
André Victor <andrevictor50@gmail.com>
2020-05-13 09:55:11 -0300

    ⚒️ : fixed the day availability query

---
+++ src/modules/appointments/services/ListProviderAppointments.ts
@@ -0,0 +1,37 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppontmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppontmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        month,
+        year,
+      },
+    );
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
