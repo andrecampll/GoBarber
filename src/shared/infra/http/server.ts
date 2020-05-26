dc32e1989dfa2d42c775cb8ebbe556ee5a9f65e3
André Victor <andrevictor50@gmail.com>
2020-05-26 09:19:13 -0300

    ✏️ : altering rate limiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,11 +16,11 @@ import '@shared/container';
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+app.use(rateLimiter);
 
 app.use(errors());
 
